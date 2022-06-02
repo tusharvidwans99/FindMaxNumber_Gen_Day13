f2eeeba04666fa09cfbada7fc2f46e13ff20a447
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-03 00:06:52 +0530

    [Tushar Vidwans] Finding the Maximum number from array of type int, float, string using Generics

---
+++ GenericMaxValue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class GenericMaxValue<T> where T : IComparable
+    {
+
+        public T[] value;
+
+        public GenericMaxValue(T[] value)
+        {
+            this.value = value;
+        }
+
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+
+        public T MaxValue(T[] values)
+        {
+            var sorted_value = Sort(values);
+            return sorted_value[^1];
+
+        }
+
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine($"Max Value is: {max}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,9 +6,21 @@
         {
             Console.WriteLine("Welcome to this program where we Finding the largest number: ");
             MaxNumber maxNumber = new MaxNumber();
-            Console.WriteLine(maxNumber.MaxNumberIs(99, 100, 101));
-            Console.WriteLine(maxNumber.MaxNumberIs(99.5, 99.3, 34.3));
-            Console.WriteLine(maxNumber.MaxNumberIs("22","35","74"));
+            //Console.WriteLine(maxNumber.MaxNumberIs(99, 100, 101));
+            //Console.WriteLine(maxNumber.MaxNumberIs(99.5, 99.3, 34.3));
+            //Console.WriteLine(maxNumber.MaxNumberIs("22","35","74"));
+
+            int[] intArray = { 99, 100, 101, 543, 785 };
+            GenericMaxValue<int> genericMaxValue = new GenericMaxValue<int>(intArray);
+            genericMaxValue.PrintMaxValue();
+
+            double[] doubles = { 99.5, 99.3, 34.3, 45.4, 666.8 };
+            GenericMaxValue<double> genericMaxValue1 = new GenericMaxValue<double>(doubles);
+            genericMaxValue1.PrintMaxValue();
+
+            string[] stringArray = { "22", "35", "74", "557","433" };
+            GenericMaxValue<string> genericMaxValue2 = new GenericMaxValue<string>(stringArray);
+            genericMaxValue2.PrintMaxValue();
         }
     }
 }
