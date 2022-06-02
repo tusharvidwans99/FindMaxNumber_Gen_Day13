87f1127c540d8cc6db7265cd883fb211be6ad866
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-02 23:03:41 +0530

    [Tushar Vidwans] Finding the Maximum int number by following all the 3 test cases.

---
+++ MaxNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class MaxNumber
+    {
+
+        public int MaxNumberIs(int firstvalue, int secondvalue, int thirdvalue)
+        {
+
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            }
+            else if (secondvalue.CompareTo(thirdvalue) > 0 && secondvalue.CompareTo(firstvalue) > 0)
+            {
+                return secondvalue;
+            }
+            else if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            else
+            {
+                Console.WriteLine("first value, second value & third value are same.");
+                return 0;
+            }
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to this program where we Finding the largest number: ");
- 
+            MaxNumber maxNumber = new MaxNumber();
+            Console.WriteLine(maxNumber.MaxNumberIs(99, 100, 101));
+
         }
     }
 }
